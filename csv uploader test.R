85be22554628c9c76848dc5536420229a22336ac
dudeandu <dudeandu@hotmail.com>
2020-06-16 14:09:43 -0400

    Commit test to github

--- csv uploader test.R
+++ csv uploader test.R
@@ -14,6 +14,8 @@ write.csv(data_canada_RAW,"canada_data_raw.csv", row.names = FALSE)
 data_heathlunits_RAW <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQa6KKgPMCZXNQpp4In8818G61Mb91-xbmng3uwh07plSPiE-TL_kbVU4ombgiDNaPXR624jMfMl2n0/pub?gid=1056686813&single=true&output=csv")
 head(data_heathlunits_RAW)
 str(data_heathlunits_RAW)
+write.csv(data_canada_RAW,"data_heathlunits_raw.csv", row.names = FALSE)
 
-
+########
 install_github("dudeandu/canada_covid")
+git remote add origin git@github.com:dudeandu/gituploader_test.git

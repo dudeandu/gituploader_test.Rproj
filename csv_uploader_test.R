2d5b2e4f4c3b759523a80657b1060aa2329e4ef0
dudeandu <dudeandu@hotmail.com>
2020-06-16 16:41:50 -0400

    preparing to run on Command line

--- csv_uploader_test.R
+++ csv_uploader_test.R
@@ -6,14 +6,14 @@
 ## install_github("dudeandu/canada_covid")
 
 data_canada_RAW <- read.csv("https://health-infobase.canada.ca/src/data/covidLive/covid19.csv")
-head(data_canada_RAW)
-str(data_canada_RAW)
+## head(data_canada_RAW)
+## str(data_canada_RAW)
 write.csv(data_canada_RAW,"canada_data_raw.csv", row.names = FALSE)
 
 
 data_heathlunits_RAW <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQa6KKgPMCZXNQpp4In8818G61Mb91-xbmng3uwh07plSPiE-TL_kbVU4ombgiDNaPXR624jMfMl2n0/pub?gid=1056686813&single=true&output=csv")
-head(data_heathlunits_RAW)
-str(data_heathlunits_RAW)
+## head(data_heathlunits_RAW)
+## str(data_heathlunits_RAW)
 write.csv(data_canada_RAW,"data_heathlunits_raw.csv", row.names = FALSE)
 
 
